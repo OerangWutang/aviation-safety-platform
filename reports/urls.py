e6c3908525179884b4f5f3d41959a6a083ff020b
OerangWutang <181835338+OerangWutang@users.noreply.github.com>
2026-05-20 14:28:42 +0200

    Delete reports directory

--- reports/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ReportListCreateView, ReportDetailView, ReportTransitionView
-
-urlpatterns = [
-    path("reports/", ReportListCreateView.as_view(), name="report_list_create"),
-    path("reports/<uuid:id>/", ReportDetailView.as_view(), name="report_detail"),
-    path("reports/<uuid:id>/transition/", ReportTransitionView.as_view(), name="report_transition"),
-]
