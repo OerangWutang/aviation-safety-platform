82ca1866650a91c5dbe351e88158c09548ad290e
OerangWutang <181835338+OerangWutang@users.noreply.github.com>
2026-05-20 14:51:55 +0200

    Delete config directory

--- config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('reports.urls')),
-    path('api/', include('organizations.urls', namespace='organizations')),
-    path('', include('core.urls')),
-]
--- config/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""
-WSGI config for aviation safety platform.
-"""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
-
-application = get_wsgi_application()
